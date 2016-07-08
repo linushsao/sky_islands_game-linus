205388813d2ac63843e5478741a0d321d4f0241d
LinusHsao <linushsao@gmail.com>
2016-07-08 11:49:22 +0000

    add abm and something

--- mods/linus_added/linus_added/init.lua
+++ mods/linus_added/linus_added/init.lua
@@ -1,1 +1,14 @@
 -- for covering old version item
+
+minetest.register_alias("default:cloud", "default:water_source")
+
+minetest.register_abm({
+	nodenames = {"default:cloud"},
+--	neighbors = {"group:water"},
+	interval = 1,
+	chance = 1,
+	catch_up = false,
+	action = function(pos, node)
+		minetest.set_node(pos, {name = "default:water_source"})
+	end
+})
